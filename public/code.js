71c93bd83dafc3b72438419d3738208fc1cedc1b
Brennyn Simpson <brennynsimpson@Brennyns-MacBook-Air.local>
2020-04-16 16:33:38 -0600

    made changes to server

--- public/code.js
+++ public/code.js
@@ -73,7 +73,7 @@ var createPaymentMethodAndCustomer = function(stripe, card) {
 async function createCustomer(paymentMethod, cardholderEmail) {
     var cardholderPlan = document.querySelector('#plan_selector').value
 
-  return fetch('/create-customer', {
+  return fetch('https://git.heroku.com/storage-facility-project.git/create-customer', {
     method: 'post',
     headers: {
       'Content-Type': 'application/json'
@@ -123,7 +123,7 @@ function handleSubscription(subscription) {
 }
 
 function confirmSubscription(subscriptionId) {
-  return fetch('/subscription', {
+  return fetch('https://git.heroku.com/storage-facility-project.git/subscription', {
     method: 'post',
     headers: {
       'Content-type': 'application/json'
@@ -141,7 +141,7 @@ function confirmSubscription(subscriptionId) {
 }
 
 function getPublicKey() {
-  return fetch('/public-key', {
+  return fetch('https://git.heroku.com/storage-facility-project.git/public-key', {
     method: 'get',
     headers: {
       'Content-Type': 'application/json'
@@ -527,21 +527,28 @@ var app = new Vue ({
 
         // Stripe Methods
         getClientInvoices: function (stripeId) {
-          var req_body = {
-            customer_id: stripeId
-          }
-          fetch( `https://git.heroku.com/storage-facility-project.git/charges`, {
-            method: "POST",
-            headers: {
-              "Content-type": "application/json"
-            },
-            body: JSON.stringify( req_body )
-          }).then( ( response ) => {
-            response.json(  ).then( ( invoices ) => {
-              this.dialog_client.first_invoice = invoices[0];
-              this.dialog_client.invoices =  invoices;
+          // var req_body = {
+          //   customer_id: stripeId
+          // }
+          fetch( `https://git.heroku.com/storage-facility-project.git/charges/${stripeId}` ).then( ( response ) => {
+              response.json(  ).then( ( invoices ) => {
+                // this.selected_unit.client = data.client;
+                this.dialog_client.first_invoice = invoices[0];
+                this.dialog_client.invoices =  invoices;
+              });
             });
-          });
+          // fetch( `https://git.heroku.com/storage-facility-project.git/charges/${stripeId}`, {
+          //   method: "POST",
+          //   headers: {
+          //     "Content-type": "application/json"
+          //   },
+          //   body: JSON.stringify( req_body )
+          // }).then( ( response ) => {
+          //   response.json(  ).then( ( invoices ) => {
+          //     this.dialog_client.first_invoice = invoices[0];
+          //     this.dialog_client.invoices =  invoices;
+          //   });
+          // });
         },
 
         // Clients Methods
