e33a3b8458ac575f0796bc0dfd38c6325901ee71
Brennyn Simpson <brennynsimpson@Brennyns-MacBook-Air.local>
2020-04-16 16:27:20 -0600

    made changes to server

--- server.js
+++ server.js
@@ -119,24 +119,25 @@ server.post("/customers", function(req, res){
 });
 
 // POST new Charge
-server.post("/charges", function(req, res) {
-  if(req.body.payment_id != null) {
-    stripe.charges.retrieve( req.body.payment_id, function(err, charge) {
-        // asynchronously called
-        if(charge != null){
-          res.status(200);
-          res.json( charge );
-        } else {
-          var response = {
-            msg: err.message
-          };
-          res.status( 400 );
-          res.json( response );
+server.get("/charges/:id", function(req, res) {
+  // if(req.body.payment_id != null) {
+  //   stripe.charges.retrieve( req.body.payment_id, function(err, charge) {
+  //       // asynchronously called
+  //       if(charge != null){
+  //         res.status(200);
+  //         res.json( charge );
+  //       } else {
+  //         var response = {
+  //           msg: err.message
+  //         };
+  //         res.status( 400 );
+  //         res.json( response );
   
-        }
-      });
-  } else if(req.body.customer_id != null){
-    stripe.charges.list( {customer: req.body.customer_id}, function(err, charges) {
+  //       }
+  //     });
+  // } else 
+  if(req.params.id != null){
+    stripe.charges.list( {customer: req.params.id}, function(err, charges) {
       // asynchronously called
       if(charges.data != null){
         res.status(200);
